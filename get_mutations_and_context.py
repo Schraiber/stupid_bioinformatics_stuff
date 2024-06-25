d4197cc43cfd5d5f1ad8237aa857fa0b5bbabb2f
Joshua Schraiber <schraibe@endeavour2.hpc.usc.edu>
2024-06-25 13:53:31 -0700

    added simlified context script

---
+++ get_mutations_and_context.py
@@ -0,0 +1,70 @@
+import argparse
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from collections import defaultdict
+import warnings
+from Bio import BiopythonWarning
+import math
+
+warnings.simplefilter("error", BiopythonWarning)
+
+def parse_bed_file(bed_file):
+    regions = []
+    with open(bed_file, 'r') as f:
+        for line in f:
+            fields = line.strip().split()
+            chrom, start, end = fields
+        regions.append([chrom,start,end])
+    return exons
+
+parser = argparse.ArgumentParser(description="Extract all possible mutations from a BED file. NOTE THAT POSITIONS ARE 0 BASED")
+parser.add_argument("reference_genome", help="Path to the reference genome in FASTA format.")
+parser.add_argument("bed_file", help="Path to the BED file containing coordinates.")
+parser.add_argument("output_tsv", help="Path to the output tsv file")
+
+args = parser.parse_args()
+
+print("Parsing reference fasta")
+
+fasta = SeqIO.to_dict(SeqIO.parse(args.reference_genome, "fasta"))
+
+print("Parsing bed file")
+
+exons = parse_bed_file(args.bed_file)
+
+outfile = open(args.output_tsv,"w")
+
+outfile.write("chrom\tpos\tref\talt\tref_trinuc\talt_trinuc\n")
+
+print("Generating output")
+
+for enst in exons:
+    seq = ""
+    before_seq = ""
+    after_seq = ""
+    pos = []
+    for region in regions:
+        chrom, start, end = region
+
+        seq += fasta[chrom].seq[start:end]
+        before_seq += fasta[chrom].seq[(start-1):(end-1)]
+        after_seq += fasta[chrom].seq[(start+1):(end+1)]
+
+        pos.extend(list(range(start,end)))
+    
+
+    for i in range(len(seq)):
+        start = pos[0]
+        stop = pos[-1]
+        cur_pos = pos[i]
+        cur_nuc = seq[i]
+        num_nuc = len(seq)
+            
+        trinuc = before_seq[i] + cur_nuc + after_seq[i]
+
+        for alt_nuc in {"A","C","G","T"} - {cur_nuc}:
+            alt_trinuc = before_seq[i] + alt_nuc + after_seq[i] 
+            outfile.write(f'{chrom}\t{cur_pos}\t{cur_nuc}\t{alt_nuc}\t{trinuc}\t{alt_trinuc}\n')
+
+#NOTE THAT OUTPUT POSITIONS ARE ZERO BASED!!!
